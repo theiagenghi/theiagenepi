71a32936134732002b098ed9c93d956ac01db460
Jake Heath <jheath@chanzuckerberg.com>
2024-07-31 10:17:57 -0700

    get staging working

--- .happy/terraform/envs/staging/providers.tf
+++ .happy/terraform/envs/staging/providers.tf
@@ -1,7 +1,4 @@
 provider "aws" {
-  region = "us-west-2"
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role}"
-  }
+  region              = "us-west-2"
   allowed_account_ids = [var.aws_account_id]
 }
--- .happy/terraform/envs/staging/versions.tf
+++ .happy/terraform/envs/staging/versions.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    region   = "us-west-2"
-    bucket   = "genepi-prod-s3-tf-state-prod-prod-genepi-stacks-state"
-    key      = "terraform.tfstate"
-    role_arn = "arn:aws:iam::829407189049:role/tfe-si"
-    encrypt  = "true"
+    region  = "us-west-2"
+    bucket  = "genepi-prod-s3-tf-state-prod-prod-genepi-stacks-state"
+    key     = "terraform.tfstate"
+    profile = "theia"
+    encrypt = "true"
 
     dynamodb_table = "genepi-prod-s3-tf-state-prod-prod-genepi-stacks-state-lock"
   }
