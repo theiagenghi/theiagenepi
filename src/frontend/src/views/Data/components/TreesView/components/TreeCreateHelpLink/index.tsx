e666a00e043156627f40bfea235586a1f5af613c
thanhleviet <thanh@cloudbinfies.com>
2024-10-18 15:15:21 +0000

    update help link for how to create my own tree

--- src/frontend/src/views/Data/components/TreesView/components/TreeCreateHelpLink/index.tsx
+++ src/frontend/src/views/Data/components/TreesView/components/TreeCreateHelpLink/index.tsx
@@ -3,7 +3,7 @@ import { StyledDiv, StyledNewTabLink, StyledSpan } from "./styles";
 
 const TreeCreateHelpLink = (): JSX.Element => {
   const HREF =
-    "https://help.theiagenepi.org/hc/en-us/articles/6712563575956-Build-on-demand-trees#customizing";
+    "https://theiagenepi.zendesk.com/hc/en-us/articles/29454978786715-Build-on-demand-trees#customizing";
   return (
     <StyledDiv>
       <StyledNewTabLink href={HREF}>
